132a497884340feab8dea4b48e57582d63e4c67d
Morhaus <alexandre.kirszenberg@gmail.com>
2015-03-05 18:39:49 +0100

    Newlines and fix index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,10 @@ import ChartDataGoogleDataAdapter from './adapters/ChartDataGoogleDataAdapter';
 import ChartData from './models/ChartData';
 import Panel from './components/panel/panel';
 import Chart from './components/charts/chart';
-import LineChart from './components/charts/line-chart';
-import PieChart from './components/charts/pie-chart';
 import Table from './components/table';
 
 export default {
   React,
   Panel,
-  Table,
-  Charts: {
-    LineChart,
-    PieChart
-  }
+  Table
 };
