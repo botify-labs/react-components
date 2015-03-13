c3581612cae553e75f059d4defb2a6a5253bbf29
Morhaus <alexandre.kirszenberg@gmail.com>
2015-03-13 14:58:00 +0100

    Improve TooltipTable row rendering and add comments

--- src/components/tooltip/tooltip-table.jsx
+++ src/components/tooltip/tooltip-table.jsx
@@ -2,13 +2,32 @@ import React from 'react/addons';
 
 import './tooltip-table.scss';
 
+/**
+ * Renders a 2xN table from its props
+ * Primary purpose is to represent chart data in tooltips
+ */
 var TooltipTable = React.createClass({
 
   displayName: 'TooltipTable',
 
   propTypes: {
-    groups: React.PropTypes.arrayOf(React.PropTypes.array),
+    // Data rows are separated in two categories: groups and metrics.
+    // Their format is the same, they only differ in style.
+    groups: React.PropTypes.arrayOf(React.PropTypes.array), // [[label, value], ...]
     metrics: React.PropTypes.arrayOf(React.PropTypes.array)
+  },
+
+  _renderRow(row, idx) {
+    return (
+      <tr key={idx} className="TooltipTable-cell">
+        <td className="TooltipTable-cell-label">
+          {row[0]}
+        </td>
+        <td className="TooltipTable-cell-value">
+          {row[1]}
+        </td>
+      </tr>
+    );
   },
 
   render() {
@@ -16,28 +35,10 @@ var TooltipTable = React.createClass({
       <div className="TooltipTable">
         <table>
           <tbody className="TooltipTable-groups">
-            {this.props.groups && this.props.groups.map((group, idx) => (
-              <tr key={idx} className="TooltipTable-cell">
-                <td className="TooltipTable-cell-label">
-                  {group[0]}
-                </td>
-                <td className="TooltipTable-cell-value">
-                  {group[1]}
-                </td>
-              </tr>
-            ))}
+            {this.props.groups && this.props.groups.map(this._renderRow)}
           </tbody>
           <tbody className="TooltipTable-metrics">
-            {this.props.metrics && this.props.metrics.map((metric, idx) => (
-              <tr key={idx} className="TooltipTable-cell">
-                <td className="TooltipTable-cell-label">
-                  {metric[0]}
-                </td>
-                <td className="TooltipTable-cell-value">
-                  {metric[1]}
-                </td>
-              </tr>
-            ))}
+            {this.props.metrics && this.props.metrics.map(this._renderRow)}
           </tbody>
         </table>
       </div>
