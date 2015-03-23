ff23ce3347f62c7c3da2cc14bfe2ada42f78712d
Morhaus <alexandre.kirszenberg@gmail.com>
2015-03-23 15:56:18 +0100

    Add comments

--- src/models/VennData.js
+++ src/models/VennData.js
@@ -7,11 +7,20 @@ class VennData {
     this.intersections = OrderedMap();
   }
 
+  /**
+   * Adds a VennSet to the list of sets
+   * @param {VennSet} set
+   */
   addSet(set) {
     this._testSet(set);
     this.sets = this.sets.add(set);
   }
 
+  /**
+   * Adds a VennIntersection to the list of intersections
+   * @param {Set<VennSet>}      sets
+   * @param {VennIntersection}  intersection  Intersection of `sets`
+   */
   addIntersection(sets, intersection) {
     if(!Set.isSet(sets)){
       throw new TypeError('sets argument is not a Set');
@@ -26,6 +35,12 @@ class VennData {
     this.intersections = this.intersections.set(sets, intersection);
   }
 
+  /**
+   * Return the size of the given VennSet or VennIntersection
+   * @param  {VennSet|VennIntersection} set
+   * @param  {Boolean}                  inclusive
+   * @return {Number}
+   */
   getSizeOf(set, inclusive = false) {
     let size = set.get('size');
 
@@ -57,14 +72,27 @@ class VennData {
     return size;
   }
 
+  /**
+   * Return the list of sets as an ordered sequence
+   * @return {IndexedSeq<VennSet>}
+   */
   getSets() {
     return this.sets.valueSeq();
   }
 
+  /**
+   * Return the list of intersections as an ordered sequence of [Set<VennSet>, VennIntersection] tuples
+   * @return {IndexedSeq<Array<Any>>}
+   */
   getIntersections() {
     return this.intersections.entrySeq();
   }
 
+  /**
+   * Return the intersection of the given list of sets
+   * @param  {Array<VennSet>} sets
+   * @return {VennIntersection}
+   */
   getIntersection(...sets) {
     return this.intersections.get(Set.of(...sets));
   }
