73bfb626b433cfb13259fef55f745d5c8a0c86f1
Morhaus <alexandre.kirszenberg@gmail.com>
2015-03-24 12:45:43 +0100

    Add HorizontalGauge component

---
+++ src/components/gauges/HorizontalGauge.jsx
@@ -0,0 +1,78 @@
+import React, { PropTypes } from 'react';
+
+import HoverTooltip from '../tooltip/HoverTooltip';
+import TooltipTable from '../tooltip/TooltipTable';
+import './HorizontalGauge.scss';
+
+let stackPropType = PropTypes.shape({
+  label: PropTypes.string,
+  value: PropTypes.number,
+  color: PropTypes.string
+});
+
+const HorizontalGauge = React.createClass({
+
+  displayName: 'HorizontalGauge',
+
+  propTypes: {
+    all: stackPropType,
+    stacks: PropTypes.arrayOf(stackPropType)
+  },
+
+  getInitialState() {
+    return {
+      hasTooltip: false,
+    };
+  },
+
+  _handleMouseEnter() {
+    this.setState({
+      hasTooltip: true,
+    });
+  },
+
+  _handleMouseLeave() {
+    this.setState({
+      hasTooltip: false,
+    });
+  },
+
+  _renderTooltip() {
+    let { stacks, all } = this.props;
+    return (
+      <TooltipTable
+        groups={[[all.label, all.value]]}
+        metrics={stacks.map((stack) => [stack.label, stack.value])}
+      />
+    );
+  },
+
+  render() {
+    let { stacks, all } = this.props;
+    let { hasTooltip } = this.state;
+    return (
+      <HoverTooltip
+        onMouseEnter={this._handleMouseEnter}
+        onMouseLeave={this._handleMouseLeave}
+        hasTooltip={hasTooltip}
+        renderTooltip={this._renderTooltip}
+        className="HorizontalGauge"
+        style={{ backgroundColor: all.color }}
+      >
+        {stacks.map((stack) => (
+          <div
+            className="HorizontalGauge-stack"
+            style={{
+              width: `${stack.value / all.value * 100}%`,
+              backgroundColor: stack.color,
+            }}
+          />
+        ))}
+      </HoverTooltip>
+    );
+
+  }
+
+});
+
+export default HorizontalGauge;
