54cfd40e2a87b5d086c415a9d29cd27e0c73ab6f
Morhaus <alexandre.kirszenberg@gmail.com>
2015-03-30 15:39:22 +0200

    Add Function::bind polyfill for PhantomJS env

--- test/entry.js
+++ test/entry.js
@@ -1,3 +1,9 @@
+/*eslint-disable*/
+Function.prototype.bind = Function.prototype.bind || function(thisp, ...args) {
+  return (...args2) => this.apply(thisp, [...args, ...args2]);
+};
+/*eslint-enable*/
+
 // Run all tests that match the GREP RegExp
 var tests = require.context('.', true, GREP);
 tests.keys().forEach(tests);
