8000bef88130ef4b6208102a9d157dd2e87a869c
zallek <nicolas.fortin37@gmail.com>
2015-04-08 11:52:53 +0200

    Replace tabs by spaces

--- example/ResourceText.jsx
+++ example/ResourceText.jsx
@@ -5,15 +5,15 @@ import 'font-awesome/css/font-awesome.css';
 import ResourceText from '../src/components/resource/ResourceText';
 
 React.render(
-	<div>
-		<ResourceText
-			resourceText={{
-				text: "Example Text",
-				description: "Example description",
-				editUrl: "http://google.com"
-			}}
-			isAdmin
-		/>
+  <div>
+    <ResourceText
+      resourceText={{
+        text: "Example Text",
+        description: "Example description",
+        editUrl: "http://google.com"
+      }}
+      isAdmin
+    />
     <ResourceText
       resourceText={{
         text: "Example Text",
@@ -21,5 +21,5 @@ React.render(
         editUrl: "http://google.com"
       }}
     />
-	</div>
+  </div>
 , document.getElementById('container'));
