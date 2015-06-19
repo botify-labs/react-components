68c100096bb98ce6f1b06a4636fdc30fdf6128fb
Morhaus <alexandre.kirszenberg@gmail.com>
2015-06-19 14:56:59 +0200

    Request change on blur for number input

--- src/components/inputs/NumberInput.jsx
+++ src/components/inputs/NumberInput.jsx
@@ -14,17 +14,51 @@ const NumberInput = React.createClass({
     className: PropTypes.string,
   },
 
-  _handleChange(value) {
+  getInitialState() {
+    return {
+      hasFocus: false,
+      value: null,
+    };
+  },
+
+  _handleFocus() {
+    this.setState({
+      hasFocus: true,
+      value: this.getValue(),
+    });
+  },
+
+  _handleBlur() {
+    let { value } = this.state;
+    this.setState({
+      hasFocus: false,
+      value: null,
+    });
     this.requestChange({ $set: parseFloat(value) });
+  },
+
+  _handleChange(value) {
+    this.setState({ value });
+  },
+
+  _getTemporaryValue() {
+    let { hasFocus, value } = this.state;
+    if (hasFocus) {
+      return value;
+    }
+    return this.getValue();
   },
 
   render() {
     let { className, ...otherProps } = this.props;
+
     return (
       <input
         {...otherProps}
+        onFocus={this._handleFocus}
+        onBlur={this._handleBlur}
         className={classNames('NumberInput', className)}
-        valueLink={this.link(this.getValue(), this._handleChange)}
+        valueLink={this.link(this._getTemporaryValue(), this._handleChange)}
         type="number"
         />
     );
