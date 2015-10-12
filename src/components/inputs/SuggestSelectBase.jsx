ee33a45cf1311869c4cdd49f0ca39e84c32f280f
kamalbennani <kamalbouchboy1@hotmail.fr>
2015-10-12 10:50:01 +0200

    Modify test condition

--- src/components/inputs/SuggestSelectBase.jsx
+++ src/components/inputs/SuggestSelectBase.jsx
@@ -325,7 +325,7 @@ const SuggestSelect = React.createClass({
     //If new value, Clear select without bluring
     if (previousSelectedOptionId !== selectedOptionId) {
 
-      if (selectedOptionId) {
+      if (selectedOptionId !== null && selectedOptionId !== undefined) {
         this.clearFilterValue();
         this.closeAllGroups();
         this.closeList();
