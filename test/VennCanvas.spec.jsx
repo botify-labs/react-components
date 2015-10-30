6f516a720feeb6c327897e9ecd3b9869d41ffa34
kamalbennani <kamalbouchboy1@hotmail.fr>
2015-10-30 14:01:11 +0100

    remove unwanted test

--- test/VennCanvas.spec.jsx
+++ test/VennCanvas.spec.jsx
@@ -51,25 +51,4 @@ describe('VennCanvas', () => {
 
     unmount(vennCanvas);
   });
-
-  it('should call its `onClick` handler prop when a set is clicked on', () => {
-    const props = {
-      vennData,
-      onClick(set, idx) { },
-    };
-    const spy = expect.spyOn(props, 'onClick');
-    const vennCanvas = render(<VennCanvas {...props} />);
-
-    // Find React base DOM components (div, span, etc.) which have been assigned a onClick handler,
-    // and simulate a click on them.
-    TestUtils.findAllInRenderedTree(vennCanvas, (component) => {
-      return (TestUtils.isDOMComponent(component) && component.props.onClick);
-    }).forEach((component) => TestUtils.Simulate.click(component));
-
-    expect(spy.calls.length).toBe(testSets.length + testIntersections.length);
-    testSets.forEach((set, idx) => expect(spy.calls[idx].arguments[0]).toBe(set));
-    testIntersections.forEach((inter, idx) => expect(spy.calls[testSets.length + idx].arguments[0]).toBe(inter.intersection));
-
-    unmount(vennCanvas);
-  });
 });
