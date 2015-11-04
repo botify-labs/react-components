1246b1ce182a2de07be9005461682eaaaf8a064a
kamalbennani <kamalbouchboy1@hotmail.fr>
2015-11-04 13:02:39 +0100

    Fix lint error by type: prefer-const

--- src/components/resource/HelpTooltip.jsx
+++ src/components/resource/HelpTooltip.jsx
@@ -12,7 +12,7 @@ const HelpTooltip = React.createClass({
   },
 
   render() {
-    let tooltip = <Tooltip id="HelpTooltip">{this.props.children}</Tooltip>;
+    const tooltip = <Tooltip id="HelpTooltip">{this.props.children}</Tooltip>;
 
     return (
       // @TODO tooltip trigger should be hover and should stayed visible when user mouse is over the tooltip.
